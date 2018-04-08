5b04787a49456c9c1bef82f8a87eaf06bf8e1b6d
jenkin-du <jenkin.ahorn@gmail.com>
2018-04-08 18:57:13 +0800

    添加项目文件。

---
+++ SceneRetrieval/Program.cs
@@ -0,0 +1,29 @@
+using ESRI.ArcGIS.esriSystem;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SceneRetrieval
+{
+    static class Program
+    {
+        private static LicenseInitializer m_AOLicenseInitializer = new SceneRetrieval.LicenseInitializer();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //ESRI License Initializer generated code.
+            m_AOLicenseInitializer.InitializeApplication(new esriLicenseProductCode[] { esriLicenseProductCode.esriLicenseProductCodeBasic, esriLicenseProductCode.esriLicenseProductCodeAdvanced },
+            new esriLicenseExtensionCode[] { esriLicenseExtensionCode.esriLicenseExtensionCode3DAnalyst, esriLicenseExtensionCode.esriLicenseExtensionCodeNetwork, esriLicenseExtensionCode.esriLicenseExtensionCodeSchematics, esriLicenseExtensionCode.esriLicenseExtensionCodeArcScan, esriLicenseExtensionCode.esriLicenseExtensionCodeBusiness, esriLicenseExtensionCode.esriLicenseExtensionCodeMLE, esriLicenseExtensionCode.esriLicenseExtensionCodeSpatialAnalyst, esriLicenseExtensionCode.esriLicenseExtensionCodeCOGO, esriLicenseExtensionCode.esriLicenseExtensionCodeGeoStats, esriLicenseExtensionCode.esriLicenseExtensionCodePublisher, esriLicenseExtensionCode.esriLicenseExtensionCodeDataInteroperability, esriLicenseExtensionCode.esriLicenseExtensionCodeTracking, esriLicenseExtensionCode.esriLicenseExtensionCodeBusinessPrem });
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            //ESRI License Initializer generated code.
+            //Do not make any call to ArcObjects after ShutDownApplication()
+            m_AOLicenseInitializer.ShutdownApplication();
+        }
+    }
+}
