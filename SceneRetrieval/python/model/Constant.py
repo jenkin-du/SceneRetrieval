f09258f076e5d3479007bba4b7147c59a5114cd2
jenkin-du <jenkin.ahorn@gmail.com>
2018-04-16 20:49:31 +0800

    改变了算法，效率更高，正确率更高

--- SceneRetrieval/python/model/Constant.py
+++ SceneRetrieval/python/model/Constant.py
@@ -14,8 +14,6 @@ dataPath = os.path.dirname(os.path.dirname(
 tempPath = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.dirname(__file__))))) + "\\temp\\"
 
-# 面状要素的分割数目
-segment = 10
 
 # 单个要素匹配的精度
 precision = 0.75
