aca2e3fcf77cd1cda70359a0986d5bab9c697519
jenkin-du <jenkin.ahorn@gmail.com>
2018-05-12 16:03:09 +0800

    code

--- SceneRetrieval/model/SimilarScene.cs
+++ SceneRetrieval/model/SimilarScene.cs
@@ -9,22 +9,11 @@ namespace SceneRetrieval.model
     class SimilarScene
     {
         //找到的相似场景中的polygon的id
-        private List<String> polygonList;
+        public List<String> polygonList;
         //匹配度
         public float md = 0;
 
 
-        public List<string> PolygonList
-        {
-            get
-            {
-                return polygonList;
-            }
-
-            set
-            {
-                polygonList = value;
-            }
-        }
+     
     }
 }
